20352c9554fd7fc9a0c7a66069412321d9ca25ef
Thomas <evan.thomas@statcan.gc.ca>
2026-02-17 13:29:30 -0500

    clean-up of 00_extract gsbpm. Additional documentation of module

--- src/00_extract_gsbpm/000_convertPDF2MD.ps1
+++ src/00_extract_gsbpm/000_convertPDF2MD.ps1
@@ -1,16 +1,13 @@
 $ErrorActionPreference = "Stop"
 
-# Load ~assemblies~ and determine format integer.
-#$wdTypes = Add-Type -AssemblyName 'Microsoft.Office.Interop.Word' -Passthru
-#$wdSaveFormat = $wdTypes | Where {$_.Name -eq "wdSaveFormat"}
-#$wdSaveFormatInt = $wdSaveFormat::wdFormatDocumentDefault.value__
+## REQUIRES POWERSHELL 7 ##
 
-#
+## Load ~assemblies~ and determine integer that specifies formats in MS Word. ##
 
 # AI says necessary to load base office assemblies first.
 $baseAssemblies = Add-Type -Path "$env:WINDIR\assembly\GAC_MSIL\office\*\office.dll" -PassThru   
 
-# Find Microsoft Word assemblies.
+# Find Microsoft Word assemblies, and add to Powershell types.
 $wdWTypes = Get-ChildItem -Path "$env:windir\assembly" -Recurse -Filter "Microsoft.Office.Interop.Word*" -File | ForEach-Object {
     Add-Type -LiteralPath $_.FullName -PassThru
 }
@@ -26,40 +23,42 @@ $wdSaveFormatInt = $wdSaveFormat::wdFormatDocumentDefault.value__
 
 
 
+
+## OPEN MS OFFICE, LOOP OVER INPUT DOCMENTS AND SAVE-AS FOR EACH. ##
+
 conda activate dsBase
 
 ## THIS HAS TO BE RUN ON A LOCAL MACHINE ##
 
-#$reportsPath="data\reference_documents\ESDC_competency_framework\systems_and_architecture"
-
 $word = New-Object -ComObject Word.application
 #$docxType = 12
 
-# select those html documents missing a corresponding docx
-#$existingExcelDocs=Get-ChildItem $reportsPath\*.docx | foreach {$_.FullName}
-
-
-
-#$htmlsMissingDocxVersion=find data/ -type f | grep ".mhtml$"
-#$htmlsMissingDocxVersion=Get-ChildItem $reportsPath\*.mhtml 
-#| Where-Object {-not $existingExcelDocs.Contains($_.FullName.Replace("mhtml",'docx'))}
+# specify input/staging extensions.
 $extensionEnd = ".pdf"
 $extensionIntermediate = ".docx"
-$mhtmlDocs = Get-ChildItem -Recurse -Path .\data\raw\ | Where-Object {$_.Extension.Equals("$extensionEnd")}
-foreach ($doc in $mhtmlDocs) {
+
+# Collect all input items
+$inputDocs = Get-ChildItem -Recurse -Path .\data\0000_raw\ | Where-Object {$_.Extension.Equals("$extensionEnd")}
+foreach ($doc in $inputDocs) {
     echo $doc
+
+    # Create staging path.
     $documentPath = $doc.FullName
-    $wordOutputPath = $documentPath.Replace("$extensionEnd", "$extensionIntermediate")
-    $txtOutputPath = $documentPath.Replace("$extensionEnd", ".md").replace("raw", "output")
-    #$wordOutputPath = $documentPath.Replace(".html", ".docx")
+    $wordOutputPath = $documentPath.Replace("$extensionEnd", "$extensionIntermediate").replace("0000_raw", "0001_staging")
 
-    echo $wordOutputPath
 
+    # Open input file using Microsoft Word/Powerpoint/Etc., save-as output type.
     $document = $word.Documents.Open("$documentPath")
     $document.SaveAs($wordOutputPath, $wdSaveFormatInt)
     $document.Close()
 
+    echo $wordOutputPath
+
+    # Then convert to Strict Markdown.
+    $txtOutputPath = $documentPath.Replace("$extensionEnd", ".md").replace("0000_raw", "0002_output")
     pandoc -t markdown_strict -o $txtOutputPath $wordOutputPath
+
+    # Optional clean-up
     #rm $wordOutputPath
 }
 $word.quit()
--- src/00_extract_gsbpm/README.md
+++ src/00_extract_gsbpm/README.md
@@ -1,1 +1,13 @@
-Needs Powershell7
+# TASK
+Convert PDFs that define GSBPM into structured JSON documents
+
+# APPROACH
+ 1) Convert PDF to DOCX, then Convert DOCX to Markdown
+ 2) LLM extract from Markdown into JSON.
+
+Step 1) are accomplished with the "000_convertPDF2MD.ps1" powershell script. This script leverages Microsoft Office Word (could be converted to Powerpoint or Excel) to import a PDF document, and save it as a DOCX. Then, using Pandoc the DOCX is converted to strict Markdown.
+
+## Requirements
+ - Powershell7
+ - Microsoft Word.
+ - 
